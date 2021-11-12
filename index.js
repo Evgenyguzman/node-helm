54d02e61cdf62771240e6188da079f1d4d6b1e69
Evgenii <eguzman@yandex.ru>
2021-11-12 23:56:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`)
-  console.log('Another log')
+  //console.log('Another log')
 });
